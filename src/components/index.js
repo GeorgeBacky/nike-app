73cc522c56ea6fc58a2eb822f8466ebe68b68d47
George Bakalis <georgebak2182@gmail.com>
2023-08-24 19:59:00 +0300

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Button from "./Button";
+import PopularProductCard from "./PopularProductCard";
+import ServiceCard from "./ServiceCard";
+import ShoeCard from "./ShoeCard";
+import ReviewCard from "./ReviewCard";
+
+export { Button, PopularProductCard, ServiceCard, ShoeCard, ReviewCard };
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import Services from "./Services";
+import CustomerReviews from "./CustomerReviews";
+import Popular from "./Popular";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+    Hero,
+    Footer,
+    Services,
+    CustomerReviews,
+    Popular,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality
+}
